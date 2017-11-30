ed030218dc4cd32884d76165eeb0a49f2dd3bd60
James Muscat <james.muscat@alces-software.com>
2017-11-30 11:29:20 +0000

    Wrapper for a config file.

---
+++ lib/alces/forge/config.rb
@@ -0,0 +1,66 @@
+require 'singleton'
+require 'yaml'
+
+module Alces
+  module Forge
+    class Config
+      include Singleton
+
+      class << self
+
+        def has_key?(k)
+          config.has_key?(k)
+        end
+
+        def method_missing(s, *a, &_)
+          if config.has_key?(s)
+            config[s]
+          else
+            nil
+          end
+        end
+
+        def set(key, value)
+          config[key.to_sym] = value
+          save
+        end
+
+        private
+
+        DEFAULT_CONFIG = {
+        }
+
+        CONFIG_DIRECTORY = '~/.config/clusterware/forge'
+        CONFIG_FILE_PATH = "#{CONFIG_DIRECTORY}/config.yml"
+
+        def config
+          @config ||= DEFAULT_CONFIG.dup.tap { |cfg|
+            # Merge in config settings here
+            if File.exists?(user_config_file)
+              cfg.merge!(YAML.load_file(user_config_file))
+            end
+          }
+        end
+
+        def user_config_dir
+          File.expand_path(CONFIG_DIRECTORY)
+        end
+
+        def user_config_file
+          File.expand_path(CONFIG_FILE_PATH)
+        end
+
+        def save
+          unless Dir.exists?(user_config_dir)
+            Dir.mkdir(user_config_dir, 0700)
+          end
+          File.write(user_config_file, config.to_yaml)
+          File.chmod(0600, user_config_file)  # File may contain auth token so should not be world-readable!
+        end
+
+      end
+
+    end
+  end
+end
+
