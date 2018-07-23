8c98dc67653b5e0329c0f8544011ef575ccf6036
william <william.mccumstie@outlook.com>
2018-07-23 19:36:12 +0100

    Do not store the version in the env var
    Storing the version in the env var makes it hard to determine the
    rails public directory (as it is off the base URL).
    
    It can be assumed that the version will only ever be bumped in the event
    of a breaking change. In this case, the `forge-cli` is already tied to
    the old version and will need to be updated anyway.
    
    To make this transition easier, the env var has been renamed the
    `CACHE_URL`. Thus it is clear that the env var is used for more than
    just forge
    
    The `SSO_URL` env var has been removed as it is now inconsistent with
    the cache. This should only been required by the production server for
    the time being. A way to set it (based on env var) can be added in the
    future

--- lib/alces/forge/config.rb
+++ lib/alces/forge/config.rb
@@ -30,11 +30,11 @@ module Alces
         end
 
         def api_url
-          ENV['FL_CONFIG_FORGE_API_URL'] || ENV['cw_FORGE_API_URL'] || config[:api_url] || 'https://forge-api.alces-flight.com/v1'
+          File.join(ENV['FL_CONFIG_CACHE_URL'], 'v1') || ENV['cw_FORGE_API_URL'] || config[:api_url] || 'https://forge-api.alces-flight.com'
         end
 
         def sso_url
-          ENV['FL_CONFIG_FORGE_SSO_URL'] || ENV['cw_FORGE_SSO_URL'] || config[:sso_url] || 'https://accounts.alces-flight.com'
+          ENV['cw_FORGE_SSO_URL'] || config[:sso_url] || 'https://accounts.alces-flight.com'
         end
 
         private
