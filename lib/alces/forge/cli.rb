d9c5154d71bfcaa199aa959c2c9dfd8e555cd22f
william <william.mccumstie@outlook.com>
2018-10-10 12:46:26 +0100

    Bump the version number
    `forge` deserves to have a major release now as it is out in the wild

--- lib/alces/forge/cli.rb
+++ lib/alces/forge/cli.rb
@@ -12,7 +12,7 @@ module Alces
 
       def run
         program :name, 'forge'
-        program :version, '0.2.0'
+        program :version, '1.0.0'
         program :description, 'Alces Flight Forge CLI'
 
         command :search do |c|
